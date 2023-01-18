1eddb93454b64d7cd2299e5f15bfc5b8e7aa02de
Owen Chang <owenchang.dev@gmail.com>
2023-01-18 09:01:01 +0800

    feat: extract the table

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Box,
+  Card,
+} from '@chakra-ui/react';
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  VisibilityState,
+  ColumnOrderState,
+} from '@tanstack/react-table';
+
+import { makeData, Person } from '../../mock/makeData'
+import TableHeader from './TableHeader';
+
+const defaultColumns: ColumnDef<Person>[] = [
+  {
+    accessorKey: 'firstName',
+    cell: info => info.getValue(),
+    footer: props => props.column.id,
+  },
+  {
+    accessorFn: row => row.lastName,
+    id: 'lastName',
+    cell: info => info.getValue(),
+    header: () => <span>Last Name</span>,
+    footer: props => props.column.id,
+  },
+  {
+    accessorKey: 'age',
+    header: () => 'Age',
+    footer: props => props.column.id,
+  },
+  {
+    accessorKey: 'visits',
+    header: () => <span>Visits</span>,
+    footer: props => props.column.id,
+  },
+  {
+    accessorKey: 'status',
+    header: 'Status',
+    footer: props => props.column.id,
+  },
+  {
+    accessorKey: 'progress',
+    header: 'Profile Progress',
+    footer: props => props.column.id,
+  },
+]
+
+const defaultColumnOrder = ['progress', 'firstName', 'lastName', 'age', 'visits', 'status',]
+
+const useMockPersonTable = () => {
+  const [data] = React.useState(() => makeData(50))
+  const [columns] = React.useState(() => [...defaultColumns])
+
+  const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
+  const [columnOrder, setColumnOrder] = React.useState<ColumnOrderState>(defaultColumnOrder)
+  const [columnPinning, setColumnPinning] = React.useState({})
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      columnVisibility,
+      columnOrder,
+      columnPinning,
+    },
+    onColumnVisibilityChange: setColumnVisibility,
+    onColumnOrderChange: setColumnOrder,
+    onColumnPinningChange: setColumnPinning,
+    getCoreRowModel: getCoreRowModel(),
+    debugTable: true,
+    debugHeaders: true,
+    debugColumns: true,
+  })
+
+  return table;
+}
+
+const MainTable = () => {
+  const table = useMockPersonTable();
+
+  return (
+    <>
+      <TableHeader table={table} />
+      <Card>
+        <TableContainer>
+          <Table>
+            <Thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <Tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <Th key={header.id}>
+                      <Box>
+                        {header?.isPlaceholder
+                          ? null
+                          : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                      </Box>
+                    </Th>
+                  ))}
+                </Tr>
+              ))}
+            </Thead>
+            <Tbody>
+              {table.getRowModel().rows.slice(0, 20).map((row) => (
+                <Tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <Td key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </Td>
+                  ))}
+                </Tr>
+              ))}
+            </Tbody>
+            <Tfoot>
+              {table.getFooterGroups().map((footerGroup) => (
+                <Tr key={footerGroup.id}>
+                  {footerGroup.headers.map((header) => (
+                    <Th key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                          header.column.columnDef.footer,
+                          header.getContext()
+                        )}
+                    </Th>
+                  ))}
+                </Tr>
+              ))}
+            </Tfoot>
+          </Table>
+        </TableContainer>
+      </Card>
+    </>
+  )
+}
+
+export default MainTable;
